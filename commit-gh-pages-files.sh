4f075154c9b27ae1cffbb3b5c4a83de10a087a10
polRk <polrkov.tm@gmail.com>
2020-07-11 14:54:26 +0300

    Revert "chore: update build script"
    This reverts commit 58befc46344954a96ebbd0a4be4ee111793e2917.

--- commit-gh-pages-files.sh
+++ commit-gh-pages-files.sh
@@ -1,6 +1,2 @@
 hugo --minify
-cd public
-git add --all
-git commit -m "Publishing to gh-pages"
-cd ..
-cp CNAME public/CNAME
+cd public && git add --all && git commit -m "Publishing to gh-pages" && cd ..
